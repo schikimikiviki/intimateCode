28a3cfe5ba19030be02ad0e5f9514a3344ee6dab
schikimikiviki <viktoria.bzdyk@gmx.at>
2023-10-14 13:26:17 +0200

    initial commit

---
+++ backend/src/test/java/com/intimatecode/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intimatecode.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
